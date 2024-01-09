54aa5dd81038657a5ee0a8c3a0752413fd30239d
tatiane <tati.guisso@gmail.com>
2024-01-09 18:43:17 -0300

    first commit

---
+++ src/main/java/com/grupo16/tcfase4/TcFase4Application.java
@@ -0,0 +1,13 @@
+package com.grupo16.tcfase4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcFase4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcFase4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo16/tcfase4/TcFase4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo16.tcfase4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcFase4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
