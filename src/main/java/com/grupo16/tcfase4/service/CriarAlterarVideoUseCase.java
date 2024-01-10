636107db3d2dcb5a1cc0450f25c916af2c78eda6
tatiane <tati.guisso@gmail.com>
2024-01-09 21:15:42 -0300

    fix: adicionando annotation Service

--- src/main/java/com/grupo16/tcfase4/service/CriarAlterarVideoUseCase.java
+++ src/main/java/com/grupo16/tcfase4/service/CriarAlterarVideoUseCase.java
@@ -1,9 +1,12 @@
 package com.grupo16.tcfase4.service;
 
+import org.springframework.stereotype.Service;
+
 import com.grupo16.tcfase4.domain.Video;
 
 import reactor.core.publisher.Mono;
 
+@Service
 public class CriarAlterarVideoUseCase {
 
 	public Mono<Video> salvar(Video video) {
