4872d193714a4bdd962b113cd1ef18ae6a9ee6c4
bruno-gds <brunog.damascena@gmail.com>
2024-01-17 16:59:45 -0300

    implementando listarTodos na VideoRepositoryGateway

--- src/main/java/com/grupo16/tcfase4/gateway/VideoRepositoryGateway.java
+++ src/main/java/com/grupo16/tcfase4/gateway/VideoRepositoryGateway.java
@@ -3,8 +3,12 @@ package com.grupo16.tcfase4.gateway;
 import java.util.Optional;
 
 import com.grupo16.tcfase4.domain.Video;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface VideoRepositoryGateway {
+
+	Page<Video> listarTodos(Pageable pageable, Boolean dataPublicacao);
 
 	String salvar(Video video);
 
