56c8ffd15ad56ec42b7a0d593112189f34453630
bruno-gds <brunog.damascena@gmail.com>
2024-01-17 16:58:57 -0300

    busca ordenada por dataPublicacao na VideoRepository

--- src/main/java/com/grupo16/tcfase4/gateway/mongo/repository/VideoRepository.java
+++ src/main/java/com/grupo16/tcfase4/gateway/mongo/repository/VideoRepository.java
@@ -1,5 +1,7 @@
 package com.grupo16.tcfase4.gateway.mongo.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.grupo16.tcfase4.gateway.mongo.document.VideoDocument;
 
 @Repository
 public interface VideoRepository extends MongoRepository<VideoDocument, String>{
-
+    Page<VideoDocument> findAllByOrderByDataPublicacaoDesc(Pageable pageable);
 }
