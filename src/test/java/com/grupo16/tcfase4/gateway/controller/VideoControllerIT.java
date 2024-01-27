8837df4a04685faac65fe3b08e5f962f858575a8
tatiane <tati.guisso@gmail.com>
2024-01-27 20:02:53 -0300

    implementando teste integrado controller

--- src/test/java/com/grupo16/tcfase4/gateway/controller/VideoControllerIT.java
+++ src/test/java/com/grupo16/tcfase4/gateway/controller/VideoControllerIT.java
@@ -1,6 +1,7 @@
 package com.grupo16.tcfase4.gateway.controller;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -54,20 +55,30 @@ class VideoControllerIT {
     @MockBean
 	private ObterEstatisticaVideoUseCase obterEstatisticaVideoUseCase;
     
+	private VideoJson videoJson = VideoJson.builder()
+	    		.id(UUID.randomUUID().toString())
+	    		.titulo("Teste")
+	    		.categoria("ACAO")
+	    		.build();
     
     @Test
     void deveSalvar()throws Exception{
-    	VideoJson videoJson = VideoJson.builder()
-    			.id(UUID.randomUUID().toString())
-    			.titulo("Teste")
-    			.categoria("ACAO")
-    			.build();
     	when(criarAlterarVideoUseCase.salvar(any(Video.class))).thenReturn(videoJson.getId());
     	
     	mockMvc.perform(MockMvcRequestBuilders.post("/videos")
 				.content(objectMapper.writeValueAsString(videoJson))
 				.contentType(MediaType.APPLICATION_JSON))
 		.andExpect(status().isCreated());
+    	
+    }
+
+    @Test
+    void deveObterUrl()throws Exception{
+    	String videoId = videoJson.getId(); 
+    	when(obterUrlVideoUseCase.obterUrl(anyString())).thenReturn("www.urlvideo.com");
+    	
+    	mockMvc.perform(MockMvcRequestBuilders.get("/videos/{id}/url", videoId))
+    			.andExpect(status().isOk());
     	
     }
 
